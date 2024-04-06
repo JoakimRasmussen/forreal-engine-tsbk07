86b627176ad15bcc6157682636011ea7aa5baec9
Robin <robwi788@student.liu.se>
2024-04-06 09:27:48 +0200

    cleaned up run

--- src/cpp/run.cpp
+++ src/cpp/run.cpp
@@ -1,41 +1,20 @@
 // uses framework Cocoa
 // uses framework OpenGL
 #define MAIN
-#define NEAR 1.0
-#define FAR 1000.0
-#define RIGHT 0.5
-#define LEFT -0.5
-#define TOP 0.5
-#define BOTTOM -0.5
-#define PI 3.14159265
-
-#include <vector>
-#include "../ext/Linux/MicroGlut.h"
-#include "GL_utilities.h"
-#include "VectorUtils4.h"
-#include "LittleOBJLoader.h"
-#include "LoadTGA.h"
-
-#include "../SimpleGUI/SimpleGUI.h"
-#include "../h/Camera.h"
-#include "../h/InputController.h"
-#include "../h/Terrain.h"
 #include "../h/GameMode.h"
-#include "../h/GUI.h"
-#include "../h/GameObject.h"
 
 GameMode* gameMode = new GameMode();
 
-void init2(void)
+void init(void)
 {
 	gameMode->init();
-	printError("init2");
+	printError("init");
 }
 
-void display2(void)
+void display(void)
 {
 	gameMode->run(0, NULL);
-	printError("display2");
+	printError("display");
 }
 
 int main(int argc, char **argv)
@@ -43,19 +22,17 @@ int main(int argc, char **argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitContextVersion(3, 2);
-	// glutInitWindowSize (1980, 1020);
 	glutInitWindowSize (Utils::windowWidth, Utils::windowHeight);
 	glutCreateWindow ("TSBK07 Project");
-	// glutDisplayFunc(display);
 	printf("GL_VERSION: %s\n", glGetString(GL_VERSION));
-	glutDisplayFunc(display2);
+	glutDisplayFunc(display);
+
 	//glutPassiveMotionFunc(InputController::handleMouseMotionBridge);
 	glutKeyboardFunc(InputController::guiKeyboardBridge);
 	glutMouseFunc(InputController::guiMouseBridge);
 	glutMotionFunc(InputController::guiDragBridge);
 	glutMouseFunc(InputController::onMouseBridge);
-	// init();
-	init2();
+	init();
 	glutRepeatingTimer(20);
 	glutMainLoop();
 	exit(0);
